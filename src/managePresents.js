574af918428285764e3ef44381cf3f11d18f433c
Batoul Alkarim <batoulalkarim1@gmail.com>
2022-04-25 10:36:22 -0400

    done

--- src/managePresents.js
+++ src/managePresents.js
@@ -1,5 +1,14 @@
 const initialState = { numberOfPresents: 0 };
+const action = {
+  type: "presents/increase"
+}
 
 export function managePresents(state = initialState, action) {
+  switch (action.type){
+    case "presents/increase":
+    return {numberOfPresents: initialState.numberOfPresents + 1};
+    default: 
+    return initialState;
+  }
   // your code here
 }
